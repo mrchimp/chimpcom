bd891ccdfa98081182b4e69689ed96594c657232
mrchimp <chimpytk@gmail.com>
2016-04-24 19:41:15 +0100

    Allow listing existing aliases

--- app/Mrchimp/Chimpcom/Commands/Alias.php
+++ app/Mrchimp/Chimpcom/Commands/Alias.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 /**
  * Add a command alias
  */
@@ -7,6 +7,7 @@ namespace Mrchimp\Chimpcom\Commands;
 
 use Auth;
 use Mrchimp\Chimpcom\Models\Alias as ChimpcomAlias;
+use Mrchimp\Chimpcom\Format;
 
 /**
  * Add a command alias
@@ -14,10 +15,26 @@ use Mrchimp\Chimpcom\Models\Alias as ChimpcomAlias;
 class Alias extends AdminCommand
 {
 
+  protected $title = 'Alias';
+  protected $description = 'View aliases or add an alias for a command.';
+  protected $usage = 'alias [&lt;alias&gt; &lt;command&gt;]';
+  protected $example = 'alias foo bar';
+
   /**
    * Run the command
    */
   public function process() {
+    if (!$this->input->get(1)) {
+        $aliases = ChimpcomAlias::get();
+        $output = [];
+        foreach ($aliases as $alias) {
+            $output[] = $alias->name;
+            $output[] = $alias->alias;
+        }
+        $this->response->say(Format::listToTable($output, 2, true));
+        return;
+    }
+
     $data = [
       'name' => $this->input->get(1),
       'alias' => implode(' ', array_slice($this->input->getParamArray(), 1))
