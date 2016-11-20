092d6eb7fd9e904e0679b56a8138d79f2a30f524
mrchimp <chimpytk@gmail.com>
2016-11-20 15:15:28 +0000

    Start getting login and memory systems working

--- app/Mrchimp/Chimpcom/Commands/LoggedInCommand.php
+++ app/Mrchimp/Chimpcom/Commands/LoggedInCommand.php
@@ -2,12 +2,16 @@
 
 namespace Mrchimp\Chimpcom\Commands;
 
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
 use Auth;
 use Mrchimp\Chimpcom\Input;
 
-abstract class LoggedInCommand extends AbstractCommand {
+abstract class LoggedInCommand extends Command {
 
-    public function run(Input $input) {
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
         if (!Auth::check()) {
             $this->response->error('You must be logged in to use this command.');
             return $this->response;
@@ -16,12 +20,13 @@ abstract class LoggedInCommand extends AbstractCommand {
         return $this->response;
     }
 
-    public function runTabcomplete(Input $input) {
-    	if (!Auth::check()) {
-    		return '';
-    	}
+    public function runTabcomplete(Input $input)
+    {
+        if (!Auth::check()) {
+            return '';
+        }
 
-    	return parent::runTabcomplete($input);
+        return parent::runTabcomplete($input);
     }
 
 }
--- app/Mrchimp/Chimpcom/Commands/Save.php
+++ app/Mrchimp/Chimpcom/Commands/Save.php
@@ -5,60 +5,100 @@
 
 namespace Mrchimp\Chimpcom\Commands;
 
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+
 use Auth;
 use Mrchimp\Chimpcom\Models\Memory;
 use Mrchimp\Chimpcom\Models\Tag;
+use Mrchimp\Chimpcom\Format;
 
 /**
  * Create a memory item
  */
-class Save extends LoggedInCommand
+class Save extends Command
 {
 
-  protected $title = 'Save';
-  protected $description = 'Save a memory. Each memory consists of a name a description. The name must be a single word. The description can be a whole paragraph. By default the memory will only be visible by you. To make it visible to other users add the --public or -p flag or after saving, use the SETPUBLIC command.<br><br>Once the memory has been saved you can search for it using FIND or SHOW and delete it with FORGET.';
-  protected $usage = 'save &lt;name&gt; &lt;description&gt; [--public|-p]';
-  protected $example = 'save chimpcom A command line website.';
-  protected $see_also = 'forget, show, find, setpublic';
+    public function configure()
+    {
+        $this->setName('save');
+        $this->setDescription('Save a memory. Each memory consists of a name a description. The name must be a single word. The description can be a whole paragraph. By default the memory will only be visible by you. To make it visible to other users add the --public or -p flag or after saving, use the SETPUBLIC command.<br><br>Once the memory has been saved you can search for it using FIND or SHOW and delete it with FORGET.');
+        $this->addUsage('chimpcom A command line website.');
+        $this->addRelated('forget');
+        $this->addRelated('show');
+        $this->addRelated('find');
+        $this->addRelated('setpublic');
 
-  /**
-   * Run the command
-   */
-  public function process() {
-    $num_params = count($this->input->getParamArray());
+        $this->addArgument(
+            'name',
+            InputArgument::REQUIRED,
+            'The name of the memory to save. Must be a single word. This does <em>not</em> need to be unique.'
+        );
 
-    if ($num_params < 2) {
-      $this->response->error('Gonna need more than that.');
-      return false;
+        $this->addArgument(
+            'content',
+            InputArgument::IS_ARRAY | InputArgument::REQUIRED,
+            'The content of the memory.'
+        );
+
+        $this->addOption(
+            'public',
+            'p',
+            null,
+            'Sets the memory as publicly visible. Otherwise only you will be able to see it.'
+        );
     }
 
-    $content = '';
-    $name = $this->input->get(1);
-    $content = implode(' ', array_slice($this->input->getParamArray(), 1));
+    /**
+     * Run the command
+     */
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        // $num_params = count($this->input->getParamArray());
+        //
+        // if ($num_params < 2) {
+        //   $this->response->error('Gonna need more than that.');
+        //   return false;
+        // }
 
-    $this->response->say('Name: ' . e($name) . '<br>');
-    $this->response->say('Content: ' . e($content) . '<br>');
+        // $content = '';
+        // $name    = $this->input->get(1);
+        // $content = implode(' ', array_slice($this->input->getParamArray(), 1));
 
-    $is_public = $this->input->isFlagSet(['--public', '-p']);
-    $user = Auth::user();
+        if (!Auth::check()) {
+            $output->write(Format::error('You must log in to use this command.'));
+            return;
+        }
 
-    $memory = new Memory();
-    $memory->name = $name;
-    $memory->content = $content;
-    $memory->user_id = $user->id;
-    $memory->public = $is_public;
+        $user = Auth::user();
 
-    if (!$memory->save()) {
-      $this->response->error('Could not save memory. Try again.');
+        $name      = $input->getArgument('name');
+        $content   = implode(' ', $input->getArgument('content'));
+        $is_public = $input->getOption('public');
+
+        $output->write('Name: ' . e($name) . '<br>');
+        $output->write('Content: ' . e($content) . '<br>');
+
+
+        $memory = new Memory();
+        $memory->name    = $name;
+        $memory->content = $content;
+        $memory->user_id = $user->id;
+        $memory->public  = $is_public;
+
+        if (!$memory->save()) {
+            $output->error('Could not save memory. Try again.');
+        }
+
+        // @todo get tags working
+        // foreach ($this->input->getTags() as $tag_word) {
+        //   $tag = new Tag();
+        //   $tag->tag = $tag_word;
+        //   $memory->tags()->save($tag);
+        // }
+
+        $output->alert('Memory saved. Id: ' . $memory->id);
     }
-
-    foreach ($this->input->getTags() as $tag_word) {
-      $tag = new Tag();
-      $tag->tag = $tag_word;
-      $memory->tags()->save($tag);
-    }
-
-    $this->response->alert('Memory saved. Id: ' . $memory->id);
-  }
 
 }
