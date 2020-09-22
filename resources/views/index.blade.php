134c70a7297e5191302520ad2f95811165b3fb58
Jake Gully <jake@maya.agency>
2020-09-23 00:41:24 +0100

    Remove old jquery include

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -42,7 +42,6 @@
 <body>
 <div id="chimpcom"></div>
 <input type="hidden" name="_token" value="<?php echo csrf_token(); ?>">
-{{-- <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script> --}}
 <script type="text/javascript" src="{{ mix('js/main.js') }}"></script>
 </body>
 </html>
