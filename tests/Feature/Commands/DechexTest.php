de0cf0e1edd4844d4e126a55739376b3ec3b588e
Jake Gully <chimpytk@gmail.com>
2020-10-21 16:51:16 +0100

    Test DECHEX command

---
+++ tests/Feature/Commands/DechexTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature\Commands;
+
+use Tests\TestCase;
+
+class DechexTest extends TestCase
+{
+    /** @test */
+    public function dechex_converts_decimal_to_hex()
+    {
+        $this->getGuestResponse('dechex 128')
+            ->assertStatus(200)
+            ->assertSee('80');
+    }
+
+    /** @test */
+    public function dexhex_can_take_css_rgb_values()
+    {
+        $response = $this->getGuestResponse('dechex rgb(0,128,255)')
+            ->assertStatus(200)
+            // ->tap(function ($response) {
+            //     dd($response->getContent());
+            // })
+            ->assertSee('#080ff');
+
+        $this->assertStringContainsString(
+            '<span style=\"color:#080ff\">',
+            $response->getContent()
+        );
+    }
+}
