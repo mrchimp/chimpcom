4d3b018e5781d1a6814c8bb9d31efb3333d251e6
Jake Gully <jake@maya.agency>
2021-10-22 22:32:49 +0100

    Add HSTS

---
+++ app/Http/Middleware/AddSecurityHeaders.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class AddSecurityHeaders
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $response = $next($request);
+
+        if (config('app.env') === 'production') {
+            $response->header('Strict-Transport-Security', 'max-age=31536000; includeSubDomains');
+        }
+
+        return $response;
+    }
+}
