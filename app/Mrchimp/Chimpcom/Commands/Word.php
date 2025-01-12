3f1d0df9e34fafb7341c9ff3d5b720132f685368
Mr Chimp <chimpytk@gmail.com>
2025-01-12 16:40:27 +0000

    Allow searching for words

--- app/Mrchimp/Chimpcom/Commands/Word.php
+++ app/Mrchimp/Chimpcom/Commands/Word.php
@@ -2,6 +2,7 @@
 
 namespace Mrchimp\Chimpcom\Commands;
 
+use Mrchimp\Chimpcom\Facades\Format;
 use Mrchimp\Chimpcom\Models\Word as WordModel;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -18,7 +19,15 @@ class Word extends Command
     {
         $this->setName('word');
         $this->setDescription('Find words');
-        $this->addArgument('search_term', InputArgument::REQUIRED, 'Word to search for');
+        $this->addArgument(
+            'search_term',
+            InputArgument::REQUIRED,
+            'Word to search for. By default, a whole-word search.'
+        );
+        $this->addOption('contains', 'c', null, 'Show words that contain the search term');
+        $this->addOption('beginning', 'b', null, 'Show words that begin with the search term');
+        $this->addOption('ending', 'e', null, 'Show words that end with the search term');
+        $this->addOption('total', 't', null, 'Only show the number of results');
     }
 
     /**
@@ -27,14 +36,40 @@ class Word extends Command
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $search_term = $input->getArgument('search_term');
-        $word = WordModel::where('word', $search_term)->first();
+        $contains = $input->getOption('contains');
+        $beginning = $input->getOption('beginning');
+        $ending = $input->getOption('ending');
+        $total = $input->getOption('total');
 
-        if (!$word) {
+        if ($contains) {
+            $search_term = '%' . $search_term . '%';
+        } elseif ($beginning) {
+            $search_term = $search_term . '%';
+        } elseif ($ending) {
+            $search_term = '%' . $search_term;
+        }
+
+        if ($contains || $beginning || $ending) {
+            $words = WordModel::where('word', 'like', $search_term)->get();
+        } else {
+            $words = WordModel::where('word', $search_term)->get();
+        }
+
+        if ($words->isEmpty()) {
             $output->error('Nothing found.');
             return 1;
         }
 
-        $output->write(e($word->word) . ' exists in the database.');
+        $output->write("Found {$words->count()} results");
+
+        if ($total) {
+            return 0;
+        }
+
+        $words->each(function ($word) use ($output) {
+            $output->write(e($word->word) . Format::nl());
+        });
+
         return 0;
     }
 }
